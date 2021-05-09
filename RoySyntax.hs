6eee4b10743578bc360aa8d9aeb59066518b0daa
xMestas <phillip.mestas51@gmail.com>
2021-05-08 19:00:38 -0700

    Add an environment that works for different types I think

--- RoySyntax.hs
+++ RoySyntax.hs
@@ -1,4 +1,15 @@
+{-# LANGUAGE ExistentialQuantification #-}
+
 module RoySyntax where
+
+class RoyDataType a where
+    litParserSymbol :: a -> String -- Doesn't like it when you don't use a in the type so just a placeholder to have it there for now.
+    parseFunction :: String -> a
+
+data DVal = forall a . RoyDataType a => DA a 
+
+type Var = String
+type Env = [(Var,DVal)]
 
 type PrimOp a = a -> a -> a
 
