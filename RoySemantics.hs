770246e09e04624ec373f2651f7acd516c311ad0
Andrew Quach <andrewquach10@gmail.com>
2021-05-11 05:27:51 -0700

    Add runtime check evals

--- RoySemantics.hs
+++ RoySemantics.hs
@@ -11,21 +11,21 @@ import Data.Maybe
 
 instance RoyDataType Int where
     litParserSymbol _ = "Int "
-    parseFunction = readMaybe
-    primOps _ = [add]
+    parseFunction     = readMaybe
+    primOps _         = [add]
 
 add :: PrimOp Int
 add = ("add",(+))
 
 readBool :: String -> Maybe Bool
-readBool ("True") = Just True
+readBool ("True")  = Just True
 readBool ("False") = Just False
-readBool _ = Nothing
+readBool _         = Nothing
 
 instance RoyDataType Bool where
     litParserSymbol _ = "Bool "
-    parseFunction = readBool
-    primOps _ = [eq]
+    parseFunction     = readBool
+    primOps _         = [eq]
 
 eq :: PrimOp Bool
 eq = ("eq",(==))
@@ -38,31 +38,64 @@ getOp n (on, f) = if n == on then Just f else Nothing
 
 findJust :: [Maybe (a -> a -> a)] -> (a -> a -> a)
 findJust ((Nothing):ls) = findJust ls
-findJust ((Just f):_) = f
+findJust ((Just f):_)   = f
 
 filterEnv :: [Var] -> (Var,DVal) -> Bool
 filterEnv vs (c,_) = elem c vs
 
 runFun :: Func -> (Env,FuncEnv) -> DVal
 runFun ((Ret e):_) m = eval e m
-runFun (s:ss) m = runFun ss (stmt s m)
+runFun (s:ss) m      = runFun ss (stmt s m)
 
 --
 -- Evaluation Functions
 --
 
 eval :: Expr -> (Env,FuncEnv) -> DVal
-eval (Lit x) _ = x
-eval (Prim n x y) m = case (eval x m, eval y m) of
-                          (DA x1, DA y1) -> DA ((findJust (map (getOp n) (primOps x1))) (fromJust (cast x1)) (fromJust (cast y1)))
-eval (Ref x) (m,_) = case (lookup x m) of (Just v) -> v
+eval (Lit x) _           = x
+eval (Prim n x y) m      = case (eval x m, eval y m) of
+                                (DA x1, DA y1) -> DA $ (findJust (map (getOp n) (primOps x1))) (fromJust (cast x1)) (fromJust (cast y1))
+eval (Ref x) (m,_)       = case (lookup x m) of (Just v) -> v
 eval (Call fn vs) (m,fm) = case (lookup fn fm) of (Just f) -> runFun f (filter (filterEnv vs) m, fm)
 
 stmt :: Stmt -> (Env,FuncEnv) -> (Env,FuncEnv)
-stmt (Set v e) (m,fm) = ((v,eval e (m,fm)):m,fm)
-stmt (If e ss) m = case (eval e m) of (DA x) -> if fromJust (cast x) then stmts ss m else m
+stmt (Set v e) (m,fm)  = ((v,eval e (m,fm)):m,fm)
+stmt (If e ss) m       = case (eval e m) of (DA x) -> if fromJust (cast x) then stmts ss m else m
 stmt (Def v ss) (m,fm) = (m,(v,ss):fm)
 
 stmts :: [Stmt] -> (Env,FuncEnv) -> (Env,FuncEnv)
 stmts (s:ss) m = stmts ss (stmt s m)
-stmts [] m = m
+stmts [] m     = m
+
+--
+-- Eval' w/ Runtime typecheck
+--
+
+runFun' :: Func -> (Env,FuncEnv) -> Maybe DVal
+runFun' (Ret e:_) m  = eval' e m
+runFun' (s:ss)    m  = stmt' s m >>= runFun' ss
+
+findJust' :: [Maybe (a -> a -> a)] -> Maybe (a -> a -> a)
+findJust' [Nothing]    = Nothing
+findJust' (Nothing:ls) = findJust' ls
+findJust' (Just f:_)   = Just f
+
+eval' :: Expr -> (Env,FuncEnv) -> Maybe DVal
+eval' (Lit x) _           = Just x
+eval' (Prim n x y) m      = do DA x1 <- eval' x m 
+                               DA y1 <- eval' y m
+                               op    <- findJust' $ map (getOp n) (primOps x1)
+                               return DA <*> (op <$> cast x1 <*> cast y1)
+eval' (Ref x) (m,_)       = lookup x m
+eval' (Call fn vs) (m,fm) = runFun' <$> lookup fn fm <*> pure (filter (filterEnv vs) m, fm) >>= id
+
+stmt' :: Stmt -> (Env,FuncEnv) -> Maybe (Env,FuncEnv)
+stmt' (Set v e) (m,fm)  = eval' e (m,fm) >>= \x -> Just ((v,x):m,fm)
+stmt' (If e ss) m       = do DA x <- eval' e m
+                             c    <- cast x
+                             if c then stmts' ss m else Just m
+stmt' (Def v ss) (m,fm) = Just (m,(v,ss):fm)
+
+stmts' :: [Stmt] -> (Env,FuncEnv) -> Maybe (Env,FuncEnv)
+stmts' (s:ss) m = stmt' s m >>= stmts' ss
+stmts' [] m     = Just m
--- RoyTests.hs
+++ RoyTests.hs
@@ -6,7 +6,7 @@ import RoySemantics
 addFunc :: Func
 addFunc = [Set "ret" (Prim "add" (Ref "i1") (Ref "i2")), Ret (Ref "ret")]
 
-quickCall :: RoyDataType a => a -> a -> Func -> Func 
+quickCall :: (RoyDataType a, RoyDataType b) => a -> b -> Func -> Func 
 quickCall x y f = [Set "i1" (Lit (DA x)), Set "i2" (Lit (DA y)), Def "fun" f, Set "ret" (Call "fun" ["i1", "i2"])]
 
 -- | Expression Evaluation Function Tests
@@ -25,6 +25,27 @@ quickCall x y f = [Set "i1" (Lit (DA x)), Set "i2" (Lit (DA y)), Def "fun" f, Se
 --
 --   >>> eval (Prim "eq" (Lit (DA (True))) (Lit (DA (False)))) ([],[])
 --   False
+--
+--   >>> eval' (Lit (DA (4::Int))) ([],[])
+--   Just 4
+--
+--   >>> eval' (Prim "add" (Lit (DA (3::Int))) (Lit (DA (4::Int)))) ([],[])
+--   Just 7
+--
+--   >>> eval' (Ref "x") ([("x",DA (5::Int))],[])
+--   Just 5
+--
+--   >>> eval' (Prim "add" (Lit (DA (3::Int))) (Ref "x")) ([("x",DA (5::Int))],[])
+--   Just 8
+--
+--   >>> eval' (Prim "eq" (Lit (DA (True))) (Lit (DA (False)))) ([],[])
+--   Just False
+--
+--   >>> eval' (Prim "add" (Lit (DA (True))) (Lit (DA (3::Int)))) ([],[])
+--   Nothing
+--
+--   >>> eval' (Prim "eq" (Lit (DA (True))) (Lit (DA (3::Int)))) ([],[])
+--   Nothing
 
 -- | Statement Evaluation Function Tests
 --
@@ -42,3 +63,24 @@ quickCall x y f = [Set "i1" (Lit (DA x)), Set "i2" (Lit (DA y)), Def "fun" f, Se
 --
 --   >>> stmts (quickCall (5::Int) (6::Int) addFunc) ([],[])
 --   ([("ret",11),("i2",6),("i1",5)],[("fun",[Set "ret" (Prim "add" (Ref "i1") (Ref "i2")),Ret (Ref "ret")])])
+--
+--   >>> stmt' (Set "x" (Lit (DA (4::Int)))) ([],[])
+--   Just ([("x",4)],[])
+--
+--   >>> stmts' [Set "x" (Lit (DA (4::Int))), Set "y" (Lit (DA True))] ([],[])
+--   Just ([("y",True),("x",4)],[])
+--
+--   >>> stmt' (If (Lit (DA True)) [Set "x" (Lit (DA False))]) ([],[])
+--   Just ([("x",False)],[])
+--
+--   >>> stmt' (Def "addInts" addFunc) ([],[])
+--   Just ([],[("addInts",[Set "ret" (Prim "add" (Ref "i1") (Ref "i2")),Ret (Ref "ret")])])
+--
+--   >>> stmts' (quickCall (5::Int) (6::Int) addFunc) ([],[])
+--   Just ([("ret",11),("i2",6),("i1",5)],[("fun",[Set "ret" (Prim "add" (Ref "i1") (Ref "i2")),Ret (Ref "ret")])])
+--
+--   >>> stmt' (If (Lit (DA (5::Int))) [Set "x" (Lit (DA False))]) ([],[])
+--   Nothing
+--
+--   >>> stmts' (quickCall (5::Int) (True) addFunc) ([],[])
+--   Nothing 
